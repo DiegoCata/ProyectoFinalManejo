66adf96530061b4b51c3696376e45fc4843509ec
Dayana-Puetate <yeceniapuetate2000@hotmail.com>
2021-01-14 22:48:46 -0500

    Creacion model

---
+++ models/model.php
@@ -0,0 +1,29 @@
+<?php
+class EnlacesPaginas
+{
+    public function enlacesPaginasModel($enlacesModel)
+    {
+        if($enlacesModel== "inicio" ||  
+        $enlacesModel== "ingreso" ||
+        $enlacesModel== "catalogo" ||
+        $enlacesModel== "catalogo_dos" ||
+        $enlacesModel== "catJoggers" ||
+        $enlacesModel== "catSF" ||
+        $enlacesModel== "precio" ||
+        $enlacesModel== "registro")
+        {
+            $module="views/modules/".$enlacesModel.".php";
+        }
+        else if($enlacesModel=="index")
+        {
+            $module="views/modules/inicio.php";
+        }
+        else
+        {
+            $module="views/modules/inicio.php";
+        }
+        return $module;
+
+    }
+}
+?>
