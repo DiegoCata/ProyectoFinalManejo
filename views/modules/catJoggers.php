c0f65a3a82f06f55050a57c1c4ffd66aa1c623b1
Paulo1603C <74008996+Paulo1603C@users.noreply.github.com>
2021-01-14 22:39:03 -0500

    Creacion de catalogos

---
+++ views/modules/catJoggers.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Basic PasswordBox - jQuery EasyUI Demo</title>
+    <LINK REL=StyleSheet HREF="css/estilo.css" TYPE="text/css" MEDIA=screen>
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.9.7/themes/bootstrap/easyui.css">
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.easyui.min.js"></script>
+</head>
+<div id="shopify-section-1599613745937" class="shopify-section index-section"><div class="rich-text">
+  
+    <div data-section-id="listaColeccion" data-section-type="seccionListaColeccion">
+    <div class="section-header section-header--small">
+    <h2 class="section-header__title">Colecciones</h2>
+  </div>
+  <div class="grid-uniform">
+      
+      <div class="grid__item small--one-half medium--one-third post-large--one-fifth text-center">
+        
+<a  class="grid-link">
+  <span class="grid-link__image grid-link__image--collection" style="height: 264px;">
+    <span class="grid-link__image-centered">
+       
+          
+<style>
+  
+
+  #CollectionImage-1003734204571--235 {
+    max-width: 235px;
+    max-height: 280.0px;
+  }
+  #CollectionImageWrapper-1003734204571--235 {
+    max-width: 235px;
+  }
+</style>
+
+
+          <div id="CollectionImageWrapper-1003734204571--235" class="collection__grid-image-wrapper supports-js">
+            <div style="padding-top:133.3333333%;">
+              <img id="CollectionImage-1003734204571--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/2.2.jpg?v=1608246249" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="0.75" data-sizes="auto" alt="Notebooks" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_180x.jpg?v=1608246249 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_235x.jpg?v=1608246249 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_360x.jpg?v=1608246249 360w">
+            </div>
+          </div>
+        
+
+        <noscript><img src="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_235x235@2x.jpg?v=1608246249" alt="Jogger" />
+        </noscript>
+      
+    </span>
+  </span>
+  <p class="grid-link__title">Jogger negro</p>
+  <p class="grid-link__title">$00.00</p>
+</a>
+      </div>
+    
+      
+      <div class="grid__item small--one-half medium--one-third post-large--one-fifth text-center">
+        
+
+<a  class="grid-link">
+  <span class="grid-link__image grid-link__image--collection" style="height: 264px;">
+    <span class="grid-link__image-centered">
+      
+<style>
+  
+
+  #CollectionImage-997779243163--235 {
+    max-width: 235px;
+    max-height: 320.0px;
+  }
+  #CollectionImageWrapper-997779243163--235 {
+    max-width: 235px;
+  }
+</style>
+
+
+          <div id="CollectionImageWrapper-997779243163--235" class="collection__grid-image-wrapper supports-js">
+            <div style="padding-top:100.0%;">
+              <img id="CollectionImage-997779243163--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/2.3.jpg?v=1602798232" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="1.0" data-sizes="auto" alt="Anillos" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_180x.png?v=1602798232 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_235x.png?v=1602798232 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_360x.png?v=1602798232 360w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_540x.png?v=1602798232 540w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_720x.png?v=1602798232 720w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_900x.png?v=1602798232 900w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_1080x.png?v=1602798232 1080w">
+            </div>
+          </div>
+        
+
+        <noscript><img src="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2019_235x235@2x.png?v=1602798232" alt="Jogger" />
+        </noscript>
+      
+    </span>
+  </span>
+  <p class="grid-link__title">Jogger palo rosa</p>
+  <p class="grid-link__title">$00.00</p>
+</a>
+      </div>
+    
+      
+      <div class="grid__item small--one-half medium--one-third post-large--one-fifth text-center">
+        
+
+<a class="grid-link">
+  <span class="grid-link__image grid-link__image--collection" style="height: 264px;">
+    <span class="grid-link__image-centered">
+        
+          
+<style>
+
+  #CollectionImage-1001916661915--235 {
+    max-width: 235px;
+    max-height: 270.0px;
+  }
+  #CollectionImageWrapper-1001916661915--235 {
+    max-width: 235px;
+  }
+</style>
+          <div id="CollectionImageWrapper-1001916661915--235" class="collection__grid-image-wrapper supports-js">
+            <div style="padding-top:100.0%;">
+              <img id="CollectionImage-1001916661915--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/2.4.jpg?v=1605225544" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="1.0" data-sizes="auto" alt="Cases" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_180x.jpg?v=1605225544 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_235x.jpg?v=1605225544 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_360x.jpg?v=1605225544 360w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_540x.jpg?v=1605225544 540w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_720x.jpg?v=1605225544 720w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_900x.jpg?v=1605225544 900w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_1080x.jpg?v=1605225544 1080w">
+            </div>
+          </div>
+          <noscript><img src="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_235x235@2x.jpg?v=1605225544" alt="Jogger" />
+        </noscript>
+      
+    </span>
+  </span>
+  <p class="grid-link__title">Jogger amarillo</p>
+  <p class="grid-link__title">$00.00</p>
+</a>
+      </div>
+    
+      
+      <div class="grid__item small--one-half medium--one-third post-large--one-fifth text-center">
+        
+
+<a class="grid-link">
+  <span class="grid-link__image grid-link__image--collection" style="height: 264px;">
+    <span class="grid-link__image-centered">
+      
+     
+<style>
+  
+
+  #CollectionImage-997779112091--235 {
+    max-width: 235px;
+    max-height: 270.0px;
+  }
+  #CollectionImageWrapper-997779112091--235 {
+    max-width: 235px;
+  }
+</style>
+
+
+          <div id="CollectionImageWrapper-997779112091--235" class="collection__grid-image-wrapper supports-js">
+            <div style="padding-top:100.0%;">
+              <img id="CollectionImage-997779112091--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/2.5.jpg?v=1602798309" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="1.0" data-sizes="auto" alt="Cadenas" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_180x.png?v=1602798309 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_235x.png?v=1602798309 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_360x.png?v=1602798309 360w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_540x.png?v=1602798309 540w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_720x.png?v=1602798309 720w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_900x.png?v=1602798309 900w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_1080x.png?v=1602798309 1080w">
+            </div>
+          </div>
+        
+
+        <noscript><img src="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/IMG_2074_235x235@2x.png?v=1602798309" alt="Jogger" />
+        </noscript>
+      
+    </span>
+  </span>
+  <p class="grid-link__title">Jogger</p>
+  <p class="grid-link__title">$00.00</p>
+</a>
+      </div>
+    
+      
+      <div class="grid__item small--one-half medium--one-third post-large--one-fifth text-center">
+        
+
+<a class="grid-link">
+  <span class="grid-link__image grid-link__image--collection" style="height: 264px;">
+    <span class="grid-link__image-centered">
+      
+        
+
+        
+          
+          
+<style>
+  
+
+  #CollectionImage-1001917808795--235 {
+    max-width: 235px;
+    max-height: 280.0px;
+  }
+  #CollectionImageWrapper-1001917808795--235 {
+    max-width: 235px;
+  }
+</style>
+          <div id="CollectionImageWrapper-1001917808795--235" class="collection__grid-image-wrapper supports-js">
+            <div style="padding-top:89.36708860759492%;">
+              <img id="CollectionImage-1001917808795--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/2.6.jpg?v=1605227826" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="1.1189801699716715" data-sizes="auto" alt="Gafas" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_b30a6e2e-1419-47a9-b852-b45b76fd39e6_180x.jpg?v=1605227826 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_b30a6e2e-1419-47a9-b852-b45b76fd39e6_235x.jpg?v=1605227826 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_b30a6e2e-1419-47a9-b852-b45b76fd39e6_360x.jpg?v=1605227826 360w">
+            </div>
+          </div>
+        
+
+        <noscript><img src="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_b30a6e2e-1419-47a9-b852-b45b76fd39e6_235x235@2x.jpg?v=1605227826" alt="Jogger" />
+        </noscript>
+      
+    </span>
+  </span>
+  <p class="grid-link__title">Jogger</p>
+  <p class="grid-link__title">$00.00</p>
+</a>
+      </div>
+    
+  </div>
+</div>
+
+
+
+
+
+
+  </div>
+  
+</div>
+
+</div>
+</html>
