46fef6e3aacbf619393aeb20fd6ace49b21ffdf9
DiegoCata <difercata26@gmail.com>
2021-01-14 22:36:30 -0500

    Clases Login, Navigation, Registro

---
+++ views/modules/navigation.php
@@ -0,0 +1,21 @@
+<nav>
+    <ul>
+        <li> <a href = "index.php?action=ingreso"> Ingreso</a> </li>
+        <li> <a href = "index.php?action=registro"> Crear cuenta</a> </li>
+    </ul>
+</nav>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a style="  width:100%;text-align:center ;color:black; padding: 50px;font-size: 15px;font-weight: bold;" href="index.php?action=inicio"><h1>Nombre_Tienda</h1></a>
+<div style="  width:100%;text-align:center;">
+<a style="word-spacing: 20pt;" href="index.php?action=inicio">Inicio </a>
+<a href="index.php?action=catalogo">Catálogo</a>
+</div>
+</body>
+</html>
