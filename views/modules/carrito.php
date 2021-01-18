d800db7f3149ec2b248673d6a69d0b3ed2e06f28
Lizbeth42 <74006880+Lizbeth42@users.noreply.github.com>
2021-01-18 14:14:04 -0500

    class carrito.php

---
+++ views/modules/carrito.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <title>Basic PasswordBox - jQuery EasyUI Demo</title>
+    <LINK REL=StyleSheet HREF="css/estilo.css" TYPE="text/css" MEDIA=screen>
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.9.7/themes/bootstrap/easyui.css">
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.easyui.min.js"></script>
+</head>
+<body>
+    
+<div id="shopify-section-1599613745937" class="shopify-section index-section">
+  <div class="rich-text">
+    <div class="section-header section-header--small">
+      <h2 style="text-indent: 130pt;" class="section-header_title rich-text_heading--medium h1">Carrito de compra</h2>
+    </div>
+    <div style="text-indent: 130pt;" class="rte rich-text__text--medium">
+      <p>Su carrito actualmete está vacío</p>
+      <p>Continúe explorando <a href="index.php?action=inicio"> aquí</a>.</p>
+    </div>
+</body>
+</html>
