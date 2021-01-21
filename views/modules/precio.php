81f5a3361bfc6eaf6969686dddd0146f77e143c9
Dayana-Puetate <yeceniapuetate2000@hotmail.com>
2021-01-21 00:31:32 -0500

    precio

--- views/modules/precio.php
+++ views/modules/precio.php
@@ -6,29 +6,46 @@
     <link rel="stylesheet" type="text/css" href="jquery-easyui-1.9.7/themes/bootstrap/easyui.css">
     <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.min.js"></script>
     <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.easyui.min.js"></script>
-    <STYLE type="text/css">
 
- </STYLE>
+  
+    <LINK REL=StyleSheet HREF="css/estilo.css" TYPE="text/css" MEDIA=screen>
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.9.7/themes/bootstrap/easyui.css">
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.easyui.min.js"></script>
+
 
 </head>
 <body>
-<div style="text-align:center ">
 
-<p><img alt="CROP TOP BLANCO A RAYAS" data-align="left" data-entity-type="file" data-entity-uuid="643c114a-eba3-47ed-be49-f318f6c298b9" height="396" src="/drupal/sites/default/files/inline-images/1.2.jpg" width="264" /><strong></strong></p>
+<p><strong><h3>CROP TOP BLANCO A RAYAS</h3></strong></p>
+<div class="grid__item small--one-half medium--one-third post-large--one-fifth ">    
+                <style>
+                  #CollectionImage-1003734204571--235 {
+                  max-width: 235px;
+                  max-height: 350.0px;
+                  }
+                  #CollectionImageWrapper-1003734204571--235 {
+                  max-width: 600px;
+                  }
+                </style>
+                <div id="CollectionImageWrapper-1003734204571--235" class="collection__grid-image-wrapper supports-js">
+                  <div style="padding-top:133.3333333%;">
+                    <img id="CollectionImage-1003734204571--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/1.2.jpg?v=1608246249" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="0.75" data-sizes="auto" alt="Crop top" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_180x.jpg?v=1608246249 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_235x.jpg?v=1608246249 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_360x.jpg?v=1608246249 360w">
+                  </div>
+                </div>
+</div>
+<div style="text-align: justify; text-indent: 170pt">
 
-<p><strong>CROP TOP BLANCO A RAYAS</strong></p>
-
-<p><strong>$12.00</strong></p>
-
-<p>Luce con un estilo vanguardista en tus salidas informales con este modelo de crop top a rayas&nbsp;confeccionada por la marca H&amp;O.&nbsp;Puedes combinar esta prenda entre jeans o shorts&nbsp;que contrasten con estilo.&nbsp;</p>
-
-<p><strong>Detalles:</strong></p>
-
+<p><strong><h2>$12.00</h2></strong></p>
+<p>Luce con un estilo vanguardista en tus salidas informales con este modelo </p><p>de crop top 
+a rayas&nbsp;confeccionada por la marca H&amp;O.&nbsp;Puedes combinar </p><p>esta prenda entre 
+jeans o shorts&nbsp;que contrasten con estilo.&nbsp;</p>
+<div style="text-align: justify; text-indent: 170pt">
+<strong>Detalles:</strong>
+</div>
 <ul>
 	<li>Blusa H&amp;O</li>
 	<li>Diseño con estampado vichy</li>
-	<li>Puños con botón</li>
-	<li>Manga larga</li>
 	<li>Cuello redondo</li>
 	<li>Elaborado en 100% viscosa</li>
 </ul>
