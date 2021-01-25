e11433201f94844c64d1c7d0b21f046e1d3b38dd
Wilder97 <74079610+Wilder97@users.noreply.github.com>
2021-01-25 13:33:34 -0500

    Precio JG1 y JG2

---
+++ views/modules/Joggers/jg1.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <title>Basic PasswordBox - jQuery EasyUI Demo</title>
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.9.7/themes/bootstrap/easyui.css">
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.easyui.min.js"></script>
+
+  
+    <LINK REL=StyleSheet HREF="css/estilo.css" TYPE="text/css" MEDIA=screen>
+    <link rel="stylesheet" type="text/css" href="jquery-easyui-1.9.7/themes/bootstrap/easyui.css">
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.min.js"></script>
+    <script type="text/javascript" src="jquery-easyui-1.9.7/jquery.easyui.min.js"></script>
+
+
+</head>
+<body>
+
+<p><strong><h3>JOGGER NEGRO</h3></strong></p>
+<div class="grid__item small--one-half medium--one-third post-large--one-fifth ">    
+                <style>
+                  #CollectionImage-997779243163--235{
+                  max-width: 235px;
+                  max-height: 350.0px;
+                  }
+                  #CollectionImageWrapper-997779243163--235{
+                  max-width: 600px;
+                  }
+                </style>
+                <div id="CollectionImageWrapper-997779243163--235" class="collection__grid-image-wrapper supports-js">
+                  <div style="padding-top:133.3333333%;">
+                    <img id="CollectionImage-997779243163--235" class="collection__grid-image lazyautosizes ls-is-cached lazyloaded" src="imagenes/dos/1.2.jpg?v=1608246249" data-widths="[180, 235, 360, 540, 720, 900, 1080, 1296, 1512, 1728, 2048]" data-aspectratio="0.75" data-sizes="auto" alt="Crop top" data-srcset="//cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_180x.jpg?v=1608246249 180w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_235x.jpg?v=1608246249 235w, //cdn.shopify.com/s/files/1/0465/6529/4235/collections/image_4f5cb8fd-36df-4e4d-9a22-6fda9e6a6718_360x.jpg?v=1608246249 360w">
+                  </div>
+                </div>
+</div>
+<div style="text-align: justify; text-indent: 170pt">
+
+<p><strong><h2>$18.50</h2></strong></p>
+<p>elaborado en base textil gruesa y suave, tiene bolsillos en los costados, </p><p> 
+cordón ajustable y elástico en pretina.&nbsp;confeccionada por la marca H&amp;O.&nbsp;Combinala como tu quieras&nbsp;</p>
+<div style="text-align: justify; text-indent: 170pt">
+<strong>Detalles:</strong>
+</div>
+<ul>
+	<li>Jogger H&amp;O</li>
+	<li>Diseño Moderno</li>
+	<li>Bastas Ajustadas</li>
+	<li>Elaborado en 100%Algodon</li>
+</ul>
+<center>
+<div class="botones">
+<button> Agregar al carrito</button>
+</center>
+</div>
+</div>
+</body>
+</html>
